c0811fb3e3c7fb34f541c80922716e9e41d2b48a
mirman-mtaggart <mtaggart@mirman.org>
2016-11-17 13:19:31 -0800

    Working GET callback

--- 5-load-from-node/index.js
+++ 5-load-from-node/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
+const fs = require("fs");
 const port = 3000;
 
 const app = express();
 
+function loadTodos(res) {
+  return fs.readFile("./todos.json", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+}
+
 app.route("/todos")
 .get((req, res) => {
-  res.send("All of the todos");
+  loadTodos(res);
 })
 .post((req, res) => {
   res.send("Creating a todo!");
+});
+
+app.route("/todos/:id")
+.get((req, res) => {
+  const id = req.params.id;
+  res.send(`Here's todo #${id}`);
+})
+.put((req, res) => {
+  const id = req.params.id;
+  res.send(`Updating todo #${id}`);
+})
+.delete((req, res) => {
+  const id = req.params.id;
+  res.send(`Deleting todo #${id}`);
 });
 
 app.listen(port, () => {
